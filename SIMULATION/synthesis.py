789c9ff6676d931fd21928ee2022b7ab004c705d
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2022-10-04 08:47:22 -0300

    synthesis functions terminated, now test

--- SIMULATION/synthesis.py
+++ SIMULATION/synthesis.py
@@ -2,6 +2,7 @@
 from collections import namedtuple
 import random as rd
 import itertools as it
+import time as tm
 
 # third party modules    ----------------------------------------------
 import numpy as np
@@ -12,23 +13,42 @@ ITER_STEP = 0.025
 RECORD_STEP = 0.1
 NUM_ITERS = 2048
 MIN_DISTANCE = 0.1
+HEAD = "bod_id,iter_num,x,y,vx,vy\n"
 
 body = namedtuple(
         "body", ["x", "y", "vx","vy"]
     )
 
-def init_planets():
+current_simu = 0
+simu_times   = {}
+def simu_time_update( step_func ):
 
-    theta_1 = rd.random()*2*np.pi
-    theta_2 = theta_1 + np.pi/3
-    theta_3 = theta_1 + np.pi*( 1 + rd.random()/3 )
-    angles = [ theta_1 , theta_2 , theta_3 ]
+    def step_with_update( bods ):
+        t = tm.time() 
+        step_func( bods )
+        dt = tm.time() - t
+        simu_times[ current_simu ] = simu_times.get( current_simu , 0 ) + dt
+
+    return step_with_update
+
+def init_planets( rnd_pos = True , rnd_speed = True ):
+    
+    angles = [ -np.pi/6 , np.pi/2 , ( 7/6 )*np.pi ] 
+    if rnd_pos:
+        theta_1 = rd.random()*2*np.pi
+        theta_2 = theta_1 + (2/3)*np.pi
+        theta_3 = theta_1 + np.pi*( 1 + (2/3)*rd.random() )
+        angles = [ theta_1 , theta_2 , theta_3 ]
+    
+    velocities = [ ( 0 , 0 ) , ( 0 , 0 ) , ( 0 , 0 ) ]
+    if rnd_speed:
+        direcs = [ 2*np.pi*rd.random() for _ in range( 3 ) ]
+        velocities = [ ( np.cos( x ) , np.sin( x ) ) for x in direcs ]
 
     bodies = []
-    for theta in angles:
+    for theta , vel in zip( angles , velocities ):
         x , y = RADIUS*np.cos( theta ) , RADIUS*np.sin( theta )
-        vel_theta = rd.random()*2*np.pi
-        vx , vy = np.cos( vel_theta ) , np.sin( vel_theta )
+        vx , vy = vel
         bod = body( x , y , vx , vy )
         bodies.append( body )
     return bodies
@@ -67,6 +87,7 @@ def get_acc_vector( body_1 , body_2 ):
     ay = acc_mod*( dy/distance )
     return ( ax , ay )
 
+@simu_time_update
 def iter_step( bodies ):
     
     acc = np.zeros( ( len( bodies ) , 2 ) )
@@ -79,4 +100,25 @@ def iter_step( bodies ):
     for i , bod in enumerate( bodies ):
         update_position( bod , acc[ i ] )
 
+def record_bod( bod, bod_id, simulation_id, iter_num ):
 
+    fl = open( f"DATA/simu/simulacao_{simulation_id}" , mode = "a" )
+    s = f"{bod_id},{iter_num},{bod.x},{bod.y},{bod.vx},{bod.vy}\n"
+    fl.write( s )
+    fl.close()
+
+    print( s )
+
+def simulate( simulation_id ):
+    
+    fl = open( f"DATA/simu/simulacao_{simulation_id}" , mode = "a" )
+    fl.write( HEAD )
+    fl.close()
+
+    bods = init_planets( rnd_speed = False )
+    for m in range( NUM_ITERS ):
+        for z in range( RECORD_STEP//ITER_STEP ):
+            iter_step( bods )
+
+        for i , bod in enumerate( bods ):
+            record_bod( bod , i, simulation_id, m ) 
