dc995afa46765a2f1b202b131cde5466fa1a8f29
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2023-01-19 13:22:09 -0300

    inicializacao dos planetas terminada

--- SIMULATION/simmu.py
+++ SIMULATION/simmu.py
@@ -8,6 +8,7 @@ class simmu:
     MIN_ITER = 2048
     MAX_ITER = 4096
 
+    RADIUS = .1
     def __init__( self , gran = 10 , max_iter = 2048 ):
         
         #----------------------------------------------
@@ -34,7 +35,7 @@ class simmu:
     def __call__( self ):
         
         if self.iteration == 0:
-            # code here
+            self.init_planets()
             pass
 
         elif self.iteration < self.max_iter:
@@ -43,8 +44,32 @@ class simmu:
 
         self.iteration += 1
         tup = (
+            self.iteration - 1,
             self.pos.copy(),
             self.vel.copy()
         )
         return tup
 
+    def init_planets( self ):
+
+        #---------------------------------------
+        # planet 1 starts in the same position
+        # for any simulation 
+        self.pos[ 0 ] = np.array([ 1. , 0 ])
+
+        #---------------------------------------
+        # planet 2 spawns in the second quadrant
+        # of the trigonometric cycle, with unitary
+        # distance to the origin
+        theta = 0.5*np.pi*( 1 + np.random.random() )
+        self.pos[ 1 ] = np.array([
+            np.cos( theta ),
+            np.sin( theta )
+        ])
+
+        #----------------------------------------
+        # the center of mass between the three
+        # planets is the origin on spawn. The starting
+        # position of the 3rd planet is deduced from
+        # the previous 2
+        self.pos[ 2 ] = -( self.pos[ 1 ] + self.pos[ 0 ] )
