b5083e12b884a706ab5d6e7889a8ca4d88bf31ea
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2023-01-23 12:20:40 -0300

    simulation class completed

--- SIMULATION/simmu.py
+++ SIMULATION/simmu.py
@@ -1,5 +1,38 @@
 import numpy as np
 
+def rmv_diagonals( mat : np.ndarray ) -> np.ndarray:
+    
+    '''
+    Removes the main diagonal of a 2d square matrix.
+    This implementation only works for 3 x 3 matrices,
+    given the scope of this project.
+
+    Example:
+
+    >>> mat = np.array([
+        [ 3 , 2. , 0 ],
+        [ 5 , 5 , 1. ],
+        [ 1. , 0 , 0 ]
+    ])
+    >>> rmv_diagonals( mat )
+    array([[2., 0.],
+           [5., 1.],
+           [1., 0.]])
+    '''
+
+    shp = mat.shape
+    if shp != ( 3 , 3 ):
+        raise ValueError( f"shape must be (3, 3) but given array has shape of {shp}")
+
+    new_mat = np.zeros( ( 3 , 2 ) )
+    new_mat[ 0 , 0 ] = mat[ 0 , 1 ]
+    new_mat[ 0 , 1 ] = mat[ 0 , 2 ]
+    new_mat[ 1 , 0 ] = mat[ 1 , 0 ]
+    new_mat[ 1 , 1 ] = mat[ 1 , 2 ]
+    new_mat[ 2 , 0 ] = mat[ 2 , 0 ]
+    new_mat[ 2 , 1 ] = mat[ 2 , 1 ]
+    return new_mat
+
 class simmu:
 
     MIN_GRAN = 10
@@ -9,6 +42,8 @@ class simmu:
     MAX_ITER = 4096
 
     RADIUS = .1
+    MAX_ACCL = ( 2*RADIUS )**( -2 )
+
     def __init__( self , gran = 10 , max_iter = 2048 ):
         
         #----------------------------------------------
@@ -92,10 +127,30 @@ class simmu:
 
         xs : np.ndarray = self.pos[ : , 0 ]
         dx = xs.reshape( ( 1 , 3 ) ) - xs.reshape( ( 3 , 1 ) )
-        dx = remove_diagonals( dx )
+        dx = rmv_diagonals( dx )
 
         ys : np.ndarray = self.pos[ : , 1 ]
         dy = ys.reshape( ( 1 , 3 ) ) - ys.reshape( ( 3 , 1 ) )
-        dy = remove_diagonals( dx )
+        dy = rmv_diagonals( dx )
 
         dist_sqr = dx**2 + dy**2
+
+        #--------------------------------------
+        # finding the modular acceleration. For
+        # numeric reasons, if the square distance
+        # is less than the squared sum of two planet
+        # radius, the aceleration is capped.
+        mod_acc = np.where(
+            dist_sqr > simmu.MAX_ACCL**( -1 ),
+            dist_sqr**( -1 ),
+            simmu.MAX_ACCL
+        )
+
+        dist = np.sqrt( dist_sqr )
+        ax   = mod_acc*( -dx/dist )
+        ay   = mod_acc*( -dy/dist )
+
+        acc = np.zeros( ( 3 , 2 ) )
+        acc[ : , 0 ] = ax.sum( axis = 1 )
+        acc[ : , 1 ] = ay.sum( axis = 1 )
+        return acc
