8daef720931b6b42581007cfa5dce78d31c28202
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2023-02-24 13:01:36 -0300

    stellar dset refatorado

--- MODEL/dsamp.py
+++ MODEL/dsamp.py
@@ -4,98 +4,93 @@ import pandas as pd
 import numpy as np
 
 from functools import lru_cache
-from random import shuffle ,choice
+from random import shuffle , choice
+from typing import *
 
-@lru_cache( 100 )
-def get_i1( n : int , i : int ):
+lru_cache( 10**5 )
+def get_tup( simu , index ):
 
-    top = n
-    bottom = 0 
-    while bottom < top - 1:
-        
-        # print( f"b , t = {bottom} , {top}" )
+    data = pd.read_csv(f"DATA/simu/simulacao_{simu}")
+    r1 = data.iloc[ index ].to_dict()
+    r2 = data.iloc[ index + 1 ].to_dict()
 
-        mid = ( top + bottom )//2
-        # print( f"m = {mid}")
-
-        mid_i = mid*n - ( mid + 1 )*mid//2
-        # print( f"mi = {mid_i}")
-
-        if mid_i > i:
-            top = mid
-        else:
-            bottom = mid
-        
-        # print( "-"*10 )
-
-    # print( str( bottom ) + "\n" )
-    return bottom
-
-def get_i2( n , i , i1 ):
+    X = tc.zeros( 12 )
+    X[ 0 ] = r1["x_0"]
+    X[ 1 ] = r1["y_0"]
+    X[ 2 ] = r1[ "vx_0" ]
+    X[ 3 ] = r1[ "vy_0" ]
+    X[ 4 ] = r1[ "x_1" ]
+    X[ 5 ] = r1[ "y_1" ]
+    X[ 6 ] = r1[ "vx_1" ]
+    X[ 7 ] = r1[ "vy_1" ]
+    X[ 8 ] = r1[ "x_2" ]
+    X[ 9 ] = r1[ "y_2" ]
+    X[ 10 ] = r1[ "vx_2" ]
+    X[ 11 ] = r1[ "vy_2" ]
     
-    bottom = i1*n - ( i1 + 1 )*i1//2
-    off_set = i - bottom
-    return off_set + ( i1 + 1 ) 
+    pos = tc.zeros( 6 )
+    pos[ 0 ] = r2[ "x_0" ]
+    pos[ 1 ] = r2[ "y_0" ]
+    pos[ 2 ] = r2[ "x_1" ]
+    pos[ 3 ] = r2[ "y_1" ]
+    pos[ 4 ] = r2[ "x_2" ]
+    pos[ 5 ] = r2[ "y_2" ]
 
+    vel = tc.zeros( 6 )
+    vel[ 0 ] = r2[ "vx_0" ]
+    vel[ 1 ] = r2[ "vy_0" ]
+    vel[ 2 ] = r2[ "vx_1" ]
+    vel[ 3 ] = r2[ "vy_1" ]
+    vel[ 4 ] = r2[ "vx_2" ]
+    vel[ 5 ] = r2[ "vy_2" ]
+
+    return X , pos , vel
+
+def get_dpoint( sizes , index  ):
+
+    #-----------------------------------------------
+    # Sizes = [ 3 , 5 , 9 ]
+    #
+    # index : 0 1 2 3 4 5 6 7 8
+    # set   : 0 0 0 1 1 2 2 2 2
+    # s_i   : 0 1 2 0 1 0 1 2 3
+
+    s_arr = sizes[ sizes <= index ]
+    simmu = sizes[ len( s_arr ) ]
+    if simmu:
+        index -= s_arr[ -1 ]
+    return simmu , index
 
 class stellarDset( Dataset ):
 
-    def __init__( self , set_num = 0 ):
+    def __init__( self , sets : List[ int ] ):
 
         # super( self ).__init__(  )
+        self.sets = sets
+        self.data_dict : Dict[ int , Tuple ] = {}
 
-        self.set_num = set_num
         try:
-            self.data = pd.read_csv(
-                f"DATA/simu/simulacao_{set_num}.csv"
+            meta : pd.DataFrame = pd.read_csv(
+                f"DATA/simu/meta.csv"
             )
+            meta.set_index( "simmu_id" , inplace = True )
         except FileNotFoundError:
-            print( f"There is no such simulation with index {set_num}")
+            print( f"meta simulation file not found")
             return
 
-        n = len( self.data )
-        self.n = n - 1
+        sizes : np.ndarray = ( meta.iloc[ self.sets ] )[ "size" ].to_numpy()
+        self.sizes = ( sizes - 1 ).cumsum()
+
+        self.n = sizes[ -1 ]
         
     def __len__( self ):
         return self.n
     
     def __getitem__(self, index ):
         
-        r1 = self.data.iloc[ index ].to_dict()
-        r2 = self.data.iloc[ index ].to_dict()
-
-        X = tc.zeros( 12 )
-        X[ 0 ] = r1["x_0"]
-        X[ 1 ] = r1["y_0"]
-        X[ 2 ] = r1[ "vx_0" ]
-        X[ 3 ] = r1[ "vy_0" ]
-        X[ 4 ] = r1[ "x_1" ]
-        X[ 5 ] = r1[ "y_1" ]
-        X[ 6 ] = r1[ "vx_1" ]
-        X[ 7 ] = r1[ "vy_1" ]
-        X[ 8 ] = r1[ "x_2" ]
-        X[ 9 ] = r1[ "y_2" ]
-        X[ 10 ] = r1[ "vx_2" ]
-        X[ 11 ] = r1[ "vy_2" ]
-        
-        pos = tc.zeros( 6 )
-        pos[ 0 ] = r2[ "x_0" ]
-        pos[ 1 ] = r2[ "y_0" ]
-        pos[ 2 ] = r2[ "x_1" ]
-        pos[ 3 ] = r2[ "y_1" ]
-        pos[ 4 ] = r2[ "x_2" ]
-        pos[ 5 ] = r2[ "y_2" ]
-
-        vel = tc.zeros( 6 )
-        vel[ 0 ] = r2[ "vx_0" ]
-        vel[ 1 ] = r2[ "vy_0" ]
-        vel[ 2 ] = r2[ "vx_1" ]
-        vel[ 3 ] = r2[ "vy_1" ]
-        vel[ 4 ] = r2[ "vx_2" ]
-        vel[ 5 ] = r2[ "vy_2" ]
-
-        return X , pos , vel
-
+        simu , index = get_dpoint( self.sizes , index )
+        simu = self.sets[ simu ]
+        return get_tup( simu , index )
 
 class sampler:
 
